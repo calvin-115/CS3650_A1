50b05a95bac69d87d6d1f993aa89038bc9b5f6d4
Calvin Liang <liang.cal@northeastern.edu>
2026-09-23 19:10:13 -0400

    Print 20x of Name and Email with loop

--- hello.c
+++ hello.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-  printf("Calvin Liang\n");
-  printf("liang.cal@northeastern.edu\n");
-
+  for (int i = 0; i < 20; i++) {
+    printf("Calvin Liang\n");
+    printf("liang.cal@northeastern.edu\n");
+  }
+  
   return 0;
 }
+
